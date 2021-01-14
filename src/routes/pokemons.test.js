13c5181912feae8f74222b6ae5824c7c19cf4aaf
antonio <antonioluiz.mg@gmail.com>
2021-01-13 21:30:59 -0300

    Create tests for pokemons routes

---
+++ src/routes/pokemons.test.js
@@ -0,0 +1,26 @@
+jest.mock('node-fetch')
+jest.mock('../db', () => ({
+  connectToMongoDb: jest.fn(),
+}))
+
+const fetch = require('node-fetch')
+const { Response } = jest.requireActual('node-fetch')
+const request = require('supertest')
+const app = require('../app')
+const charmander = {
+  id: 7,
+  name: 'Charmander',
+}
+
+describe('get pokemon', () => {
+  it('should return one pokemon', () => {
+    fetch.mockReturnValueOnce(new Response(JSON.stringify(charmander)))
+
+    return request(app)
+      .get('/players')
+      .then(({ statusCode, body }) => {
+        expect(statusCode).toBe(200)
+        expect(body).toEqual(expect.arrayContaining([player]))
+      })
+  })
+})
